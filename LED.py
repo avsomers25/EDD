9273821dabef884ec98d32764bdb7b1073023f15
avsomers25 <67980093+avsomers25@users.noreply.github.com>
2022-04-06 08:43:06 -0400

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,10 +1,26 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print("LED on")
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print("LED off")
-GPIO.output(18,GPIO.LOW)
+#red at pin 5 (29)
+#blue at pin 6 (31)
+#green at pin 12 (32)
+
+
+GPIO.SetupGPIO
+GPIO.PinMode(5,GPIO.OUTPUT)
+GPIO.PinMode(6,GPIO.OUTPUT)
+GPIO.PinMode(12,GPIO.OUTPUT)
+ 
+GPIO.DigitalWrite(5,GPIO.OFF)
+GPIO.DigitalWrite(6,GPIO.OFF)
+GPIO.DigitalWrite(12,GPIO.OFF)
+
+GPIO.DigitalWrite(5,GPIO.ON)
+GPIO.DigitalWrite(6,GPIO.ON)
+GPIO.DigitalWrite(12,GPIO.ON)
+
+time.sleep(10)
+
+GPIO.DigitalWrite(5,GPIO.OFF)
+GPIO.DigitalWrite(6,GPIO.OFF)
+GPIO.DigitalWrite(12,GPIO.OFF)
+
